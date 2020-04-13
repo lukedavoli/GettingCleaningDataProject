a58f744ac2ae473c3bf2b2436c1baa89d677209d
lukedavoli <s3782747@student.rmit.edu.au>
2020-04-13 11:55:39 +1000

    Step 5, wrote README.md

--- run_analysis.R
+++ run_analysis.R
@@ -13,14 +13,22 @@ if(!file.exists("UCI HAR Dataset"))
     unzip(filename) 
 }
 
-features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+features <-      read.table("UCI HAR Dataset/features.txt",            
+                            col.names = c("n","functions"))
+activities <-    read.table("UCI HAR Dataset/activity_labels.txt",     
+                            col.names = c("code", "activity"))
+subject_test <-  read.table("UCI HAR Dataset/test/subject_test.txt",   
+                            col.names = "subject")
+x_test <-        read.table("UCI HAR Dataset/test/X_test.txt",         
+                            col.names = features$functions)
+y_test <-        read.table("UCI HAR Dataset/test/y_test.txt",         
+                            col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", 
+                            col.names = "subject")
+x_train <-       read.table("UCI HAR Dataset/train/X_train.txt",       
+                            col.names = features$functions)
+y_train <-       read.table("UCI HAR Dataset/train/y_train.txt",       
+                            col.names = "code")
 
 #STEP 1
 X <- rbind(x_train, x_test)
@@ -38,19 +46,34 @@ step3$code <- activities[step2$code, 2]
 #STEP 4
 step4 <- step3
 names(step4)[2] <- "activity"
-names(step4)<-gsub(pattern="Acc",      replacement="Accelerometer", x=names(step3))
-names(step4)<-gsub(pattern="Gyro",     replacement="Gyroscope",     x=names(step3))
-names(step4)<-gsub(pattern="BodyBody", replacement="Body",          x=names(step3))
-names(step4)<-gsub(pattern="Mag",      replacement="Magnitude",     x=names(step3))
-names(step4)<-gsub(pattern="^t",       replacement="Time",          x=names(step3))
-names(step4)<-gsub(pattern="^f",       replacement="Frequency",     x=names(step3))
-names(step4)<-gsub(pattern="tBody",    replacement="TimeBody",      x=names(step3))
-names(step4)<-gsub(pattern="-mean()",  replacement="Mean",          x=names(step3), ignore.case = TRUE)
-names(step4)<-gsub(pattern="-std()",   replacement="STD",           x=names(step3), ignore.case = TRUE)
-names(step4)<-gsub(pattern="-freq()",  replacement="Frequency",     x=names(step3), ignore.case = TRUE)
-names(step4)<-gsub(pattern="angle",    replacement="Angle",         x=names(step3))
-names(step4)<-gsub(pattern="gravity",  replacement="Gravity",       x=names(step3))
+names(step4) <- gsub(pattern="Acc",      replacement="Accelerometer", 
+                     x=names(step4))
+names(step4) <- gsub(pattern="Gyro",     replacement="Gyroscope",     
+                     x=names(step4))
+names(step4) <- gsub(pattern="BodyBody", replacement="Body",          
+                     x=names(step4))
+names(step4) <- gsub(pattern="Mag",      replacement="Magnitude",     
+                     x=names(step4))
+names(step4) <- gsub(pattern="^t",       replacement="Time",          
+                     x=names(step4))
+names(step4) <- gsub(pattern="^f",       replacement="Frequency",     
+                     x=names(step4))
+names(step4) <- gsub(pattern="tBody",    replacement="TimeBody",      
+                     x=names(step4))
+names(step4) <- gsub(pattern="-mean()",  replacement="Mean",          
+                     x=names(step4), ignore.case = TRUE)
+names(step4) <- gsub(pattern="-std()",   replacement="STD",           
+                     x=names(step4), ignore.case = TRUE)
+names(step4) <- gsub(pattern="-freq()",  replacement="Frequency",     
+                     x=names(step4), ignore.case = TRUE)
+names(step4) <- gsub(pattern="angle",    replacement="Angle",         
+                     x=names(step4))
+names(step4) <- gsub(pattern="gravity",  replacement="Gravity",       
+                     x=names(step4))
 
-
-
+#STEP 5
+step5 <- step4
+step5 <- step5[ , names(step5) != "subject"]
+step5 <- summarise_all(group_by(step5, activity), mean)
+write.table(step5, "meanResultsByActivity.txt", row.names = FALSE)
 
